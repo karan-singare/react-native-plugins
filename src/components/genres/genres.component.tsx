e0c1f6bde6d357d6d3f19ffd8a6f56c90fbaaf8f
Karan Singare <karansingare@gmail.com>
2023-02-04 01:52:58 +0530

    initial commit

---
+++ src/components/genres/genres.component.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default class Genres extends React.Component {
+  constructor(props: any) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>This is genre plugin component</Text>
+      </View>
+    );
+  }
+}
